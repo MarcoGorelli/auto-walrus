9444157b825c85d2e5b55cc9aa8c0eda2e6aee21
MarcoGorelli <>
2022-10-07 10:29:24 +0100

    fixup

--- auto_walrus.py
+++ auto_walrus.py
@@ -1,6 +1,7 @@
 import ast
 from tokenize_rt import src_to_tokens, tokens_to_src, reversed_enumerate
 
+LINE_LENGTH = 88
 
 def first3(tokens):
     if isinstance(tokens, list):
@@ -11,7 +12,7 @@ def find_names(node, end_lineno=None, end_col_offset=None):
     names = set()
     for _node in ast.walk(node):
         if isinstance(_node, ast.Name):
-            names.add((_node.id, _node.lineno, _node.col_offset, end_lineno or _node.end_lineno, end_col_offset or _node.col_offset))
+            names.add((_node.id, _node.lineno, _node.col_offset, end_lineno or _node.end_lineno, end_col_offset or _node.end_col_offset))
     return names
 
 def visit_function_def(node):
@@ -21,17 +22,18 @@ def visit_function_def(node):
     ifs = set()
     for _node in ast.walk(node):
         names.update(find_names(_node))
+    
+    for _node in node.body:
         if isinstance(_node, ast.Assign):
-            if len(_node.targets) == 1:
+            if len(_node.targets) == 1 and isinstance(_node.targets[0], ast.Name):
                 target = _node.targets[0]
                 assignments.update(find_names(target, _node.end_lineno, _node.end_col_offset))
-        elif isinstance(_node, ast.If):
+        elif isinstance(_node, (ast.If, ast.While)):
             ifs.update(find_names(_node.test))
     
     names = sorted(names, key=lambda x: (x[1], x[2]))
     assignments = sorted(assignments, key=lambda x: (x[1], x[2]))
     ifs = sorted(ifs, key=lambda x: (x[1], x[2]))
-
     walrus = []
 
     for ass in assignments:
@@ -41,9 +43,14 @@ def visit_function_def(node):
         ifass = ifasss[0]
         asspos = first3(names).index(first3(ass))
         ifpos = first3(names).index(first3(ifass))
-        if ifpos == asspos + 1:
-            if [first3(i) for i in names if i[0] == ass[0]][0] == first3(ass):
-                # probably also need to check that this is the first usage
+        # check name hasn't been used in between assignment
+        # and if statement
+        if ass[0] not in [names[i][0] for i in range(asspos+1, ifpos)]:
+            # check the assignment was the variable's first usage
+            this_vars_assignments = [first3(i) for i in assignments if i[0] == ass[0]]
+            this_vars_names = [first3(i) for i in names if i[0] == ass[0]]
+            if (len(this_vars_assignments) == 1) and (this_vars_names[0] == first3(ass)) and len(this_vars_names) > 2:
+                # check it's actually used again, else no point in walrusing
                 walrus.append((ass, ifass))
     return walrus
     # check:
@@ -60,21 +67,44 @@ def main(file):
     for node in ast.walk(tree):
         if isinstance(node, ast.FunctionDef):
             walruses.extend(visit_function_def(node))
-
+    lines_to_remove = []
+    walruses = sorted(walruses, key=lambda x: (-x[1][1], -x[1][2]))
     for ass, if_ in walruses:
         if ass[1] != ass[3]:
             continue
         txt = lines[ass[1]-1][ass[2]:ass[4]]
         if txt.count('=') > 1:
             continue
+        line = lines[if_[1]-1]
+        # actually, if it's not used anywhere else,
+        # maybe just...replace with definition?
+        # I think we need a line-length parameter,
+        # and we don't rewrite if we exceed the line-length
+        left_bit = line[:if_[2]]
+        right_bit = line[if_[4]:]
+        no_paren = left_bit.endswith('(') and right_bit.startswith(')')
+        replace = txt.replace('=', ':=')
+        if no_paren:
+            line = left_bit + replace + right_bit
+        else:
+            line = left_bit + '(' + replace + ')' + right_bit
+        if len(line) > LINE_LENGTH:
+            # don't rewrite if it would split over multiple lines
+            continue
+        # replace assignment
         lines[ass[1]-1] = f'{lines[ass[1]-1][:ass[2]]}{lines[ass[1]-1][ass[4]:]}'
-        lines[if_[1]-1] = lines[if_[1]-1].replace(ass[0], '('+txt.replace('=', ':=')+')')
+        # add walrus
+        lines[if_[1]-1] = line
+        # remove empty line
+        if not lines[ass[1]-1].strip():
+            lines_to_remove.append(ass[1]-1)
 
-    newlines = '\n'.join(lines)
-    if newlines:
-        newlines += '\n'
+    newlines = [line for i, line in enumerate(lines) if i not in lines_to_remove]
+    newcontent = '\n'.join(newlines)
+    if newcontent:
+        newcontent += '\n'
     with open(file, 'w') as fd:
-        fd.write(newlines)
+        fd.write(newcontent)
 
 if __name__ == '__main__':
     import sys
