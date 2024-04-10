a22f0bb15afa685b5eac80bd831da672a474f80b
Marco Gorelli <33491632+MarcoGorelli@users.noreply.github.com>
2024-04-10 08:56:01 +0100

    Bump version to 0.3.1

--- utils/bump_version.py
+++ utils/bump_version.py
@@ -23,4 +23,3 @@ with open("pyproject.toml", "w", encoding="utf-8") as f:
 subprocess.run(["git", "commit", "-a", "-m", f"Bump version to {version}"])
 subprocess.run(["git", "tag", "-a", version, "-m", version])
 subprocess.run(["git", "push", "--follow-tags"])
-
