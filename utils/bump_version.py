27dbb25798026765b1d74f9f62e4c5b23f8edcdc
Christian Clauss <cclauss@me.com>
2025-03-23 07:40:10 +0100

    pre-commit: Upgrade to ruff v0.11.2

--- utils/bump_version.py
+++ utils/bump_version.py
@@ -1,5 +1,4 @@
 # mypy: ignore
-# ruff: noqa
 import re
 import subprocess
 import sys
@@ -29,6 +28,6 @@ content = content.replace(
 with open("README.md", "w", encoding="utf-8") as f:
     f.write(content)
 
-subprocess.run(["git", "commit", "-a", "-m", f"Bump version to {version}"])
-subprocess.run(["git", "tag", "-a", version, "-m", version])
-subprocess.run(["git", "push", "--follow-tags"])
+subprocess.run(["git", "commit", "-a", "-m", f"Bump version to {version}"], check=True)
+subprocess.run(["git", "tag", "-a", version, "-m", version], check=True)
+subprocess.run(["git", "push", "--follow-tags"], check=True)
